011aee532fca693ffe2f6c64e5765e833d29d67a
Isaac Perkins <isaacperkins987@gmail.com>
2024-07-10 21:42:44 -0700

    Fixed path compatability

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import pickle
 import gradio as gr
 from tensorflow.keras.layers import TextVectorization
@@ -7,12 +8,12 @@ import tensorflow as tf
 categories = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
 
 # Load the vectorizer
-vectorizer_pickle = pickle.load(open("models\\vectorizer.pkl", "rb"))
+vectorizer_pickle = pickle.load(open(os.path.join("models", "vectorizer.pkl"), "rb"))
 vectorizer = TextVectorization.from_config(vectorizer_pickle['config'])
 vectorizer.set_weights(vectorizer_pickle['weights'])
 
 # Load the model
-model = tf.keras.models.load_model('models\safe_chat.h5')
+model = tf.keras.models.load_model(os.path.join("models", "safe_chat.h5"))
 
 def predict_hate(text):
     results = model.predict(vectorizer([text]))[0] > 0.5
